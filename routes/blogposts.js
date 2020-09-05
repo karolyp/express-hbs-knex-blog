b437bc8fed466dda0250587d612f474889eb0051
Pákozdi Károly <h668139@stud.u-szeged.hu>
2020-09-05 17:50:23 +0200

    Adding POST new blogpost

--- routes/blogposts.js
+++ routes/blogposts.js
@@ -16,8 +16,56 @@ router.get('/', async function (req, res, next) {
   res.render('blogposts', { title: 'Blog posts', blogposts });
 });
 
-router.get('/new', (req, res, next) => {
-  res.render('new_blogpost');
+router.get('/new', async (req, res, next) => {
+  const categories = await knex('categories').select();
+  res.render('new_blogpost', {
+    title: 'Új blogposzt',
+    categories
+  });
 });
+
+router.post('/', async (req, res, next) => {
+  // Mi történik, ha nincs adott nevű szerző?
+  // Adjuk hozzá az adatbázishoz
+  // Ha hibás a req.body, akkor rendereljük ki az error page-et
+  // if(rossz a req.body) then szálljon el
+  const blogpostDTO = req.body;
+
+  if (!isBlogpostValid(blogpostDTO)) {
+    res.render('error', {
+      message: 'Hibás blogposzt!'
+    });
+  } else {
+    // dolgozzon tovább a jó paraméterekkel 
+    let authorId;
+    if (isAuthorExist(blogpostDTO.author)) {
+      authorId = await knex('authors')
+        .select('id')
+        .where('full_name', '=', blogpostDTO.author)
+        .first()
+    } else {
+      authorId = await knex('authors').insert({
+        full_name: blogpostDTO.author
+      }).returning('id');
+    }
+    console.log(authorId);
+  }
+});
+
+const isBlogpostValid = (blogpost) => {
+  // vannak-e/üresek-e a fieldjei?
+  return blogpost.title &&
+    blogpost.description &&
+    blogpost.author &&
+    blogpost.category;
+};
+
+const isAuthorExist = async (full_name) => {
+  const count = await knex('authors')
+    .where({ full_name })
+    .count('full_name as fn')
+    .first();
+  return count.fn > 0;
+};
 
 module.exports = router;
